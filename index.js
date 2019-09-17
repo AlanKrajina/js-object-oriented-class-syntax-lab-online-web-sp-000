b5c0c41c9b8c7cb96f24bc1e30524b33db85619f
Alan Krajina <alankrajina88@gmail.com>
2019-09-17 08:37:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,10 @@ class BoardMember {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
+
+
+class Ceo extends BoardMember {
+  hireEmployee(){
+  return "Welcome aboard!"
+  }
+}
