159b8533ad5353331ce2c1f2f4575ce04ceb434c
Alan Krajina <alankrajina88@gmail.com>
2019-09-17 08:34:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ class BoardMember {
     this.name = name;
     this.homeState = homeState;
     this.training = training;
+
+  veto (){
+    return 'No, I must disagree'
+  }
+  approve (){
+    return 'You can do that!'
+  }
+  doCharity (){
+    return "I like to help people."
   }
 
-  sayHello() {
-    console.log(`Hello, my name is ${this.name}`);
+  releasePressStatement (){
+    return "You will see great things from Scuber."
   }
+
+  sayHi (){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
+
 }
